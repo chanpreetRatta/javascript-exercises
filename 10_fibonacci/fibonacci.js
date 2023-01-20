064640920a691ea603c5bd84b6e1b816136c109a
Devolpment <devolpment@MacBook-Pro.local>
2023-01-19 20:34:19 -0800

    commit fabonacci seq

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function (value) {
+  if (value < 0) return "OOPS";
+  let fibonacciSequence = [1, 1];
+  for (let i = 2; i < value; i++) {
+    fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i - 2]);
+  }
 
+  return fibonacciSequence[fibonacciSequence.length - 1];
 };
 
 // Do not edit below this line
