386bb4bf7cb78c490c8e8d3f9c28c81c8ac33ceb
Devolpment <devolpment@MacBook-Pro.local>
2023-01-19 19:06:34 -0800

    Implement Palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let checkPalindromes = [];
+  let punctuations = ".,:!?;-_{}[]()* ".split("");
+  for (let alphabet of string.toLowerCase()) {
+    if (!punctuations.includes(alphabet)) checkPalindromes.push(alphabet);
+  }
 
+  return checkPalindromes.join("") === checkPalindromes.reverse().join("");
 };
 
 // Do not edit below this line
