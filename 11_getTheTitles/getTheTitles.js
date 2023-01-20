093bb322c8873f3328b900be30eeb0582f3db407
Devolpment <devolpment@MacBook-Pro.local>
2023-01-19 20:51:40 -0800

    implement getTheTitle function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  let booksTitles = [];
+  for (let book of books) {
+    booksTitles.push(book.title);
+  }
 
+  return booksTitles;
 };
 
 // Do not edit below this line
