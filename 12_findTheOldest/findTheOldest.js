b6157a3ad030ef629a452e2bb68852e9ed09f3b9
Devolpment <devolpment@MacBook-Pro.local>
2023-01-21 16:52:28 -0800

    Implement findTheoldestPerson function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson = people.reduce((accumulator, currentValue) => {
+    let currentOldest = getAge(
+      accumulator.yearOfDeath,
+      accumulator.yearOfBirth
+    );
+    let nextOldest = getAge(currentValue.yearOfDeath, currentValue.yearOfBirth);
 
+    return currentOldest > nextOldest ? accumulator : currentValue;
+  });
+
+  return oldestPerson;
 };
 
+function getAge(deathDate, birthDate) {
+  if (!deathDate) deathDate = new Date().getFullYear();
+
+  return deathDate - birthDate;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
