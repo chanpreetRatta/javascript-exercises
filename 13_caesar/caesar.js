eb5e25f7b9cad5b6c157eccc753086711414a33b
Devolpment <devolpment@MacBook-Pro.local>
2023-01-23 18:48:49 -0800

    Implement casesar function and passed the first test

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,10 @@
-const caesar = function() {
+const caesar = function (string, spaces) {
+  let arrayOfStrings = string.split("");
+  let returnArray = arrayOfStrings.map((latter) => {
+    return String.fromCharCode(latter.charCodeAt(latter) + spaces);
+  });
 
+  return returnArray.join();
 };
-
 // Do not edit below this line
 module.exports = caesar;
